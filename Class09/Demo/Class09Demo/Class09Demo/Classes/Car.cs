de722d7e621e1200ebb89b53951e12a0c7c21944
Iverson, Amanda <amanda@codefellows.com>
2018-06-14 16:38:14 -0700

    Class09 demo

---
+++ Class09/Demo/Class09Demo/Class09Demo/Classes/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class09Demo.Classes
+{
+    class Car
+    {
+	    public int NumberOfDoors { get; set; }
+	    public int TopSpeed { get; set; }
+	    public string Manufacturer { get; set; }
+
+	    public Color Color { get; set; }
+
+    }
+
+	enum Color
+	{
+		Red,
+		Green,
+		Blue,
+		Purple,
+		Black,
+		Yellow,
+		Shiny
+	}
+}
