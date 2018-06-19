1578edd8225aa3909a8a94170627a2c9bceffc9d
Iverson, Amanda <amanda@codefellows.com>
2018-06-19 16:44:27 -0700

    Add Class 12 demo

---
+++ Class12/Demo/DemoClass12/DemoClass12/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoClass12.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoClass12.Controllers
+{
+    public class HomeController: Controller
+    {
+	    private DemoClass12DbContext _context;
+
+	    public HomeController(DemoClass12DbContext context)
+	    {
+		    _context = context;
+
+	    }
+
+	    public IActionResult Index()
+	    {
+
+		   var authors = _context.Book.Where(n => n.Author.Length > 4);
+		    return View();
+	    }
+    }
+}
---
+++ Class12/Demo/DemoClass12/DemoClass12/Data/DemoClass12DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoClass12.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoClass12.Data
+{
+    public class DemoClass12DbContext : DbContext
+    {
+	    public DemoClass12DbContext(DbContextOptions<DemoClass12DbContext> options) : base(options)
+	    {
+		    
+	    }
+
+	    public DbSet<Book> Book { get; set; }
+    }
+}
