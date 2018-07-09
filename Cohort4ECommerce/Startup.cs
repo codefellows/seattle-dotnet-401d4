271bb4146bd01b571514f1194c26f74eb8849fd6
Iverson, Amanda <amanda@codefellows.com>
2018-07-09 15:54:14 -0700

    added middleware and runtime to allow for MVC

--- Cohort4ECommerce/Startup.cs
+++ Cohort4ECommerce/Startup.cs
@@ -15,6 +15,7 @@ namespace Cohort4ECommerce
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +25,8 @@ namespace Cohort4ECommerce
             {
                 app.UseDeveloperExceptionPage();
             }
+
+			app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
