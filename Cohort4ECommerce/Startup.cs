b496c66f8e47617b1eb63ca4f8cd5a6b0d23cc80
Iverson, Amanda <amanda@codefellows.com>
2018-07-09 17:18:22 -0700

    add files

--- Cohort4ECommerce/Startup.cs
+++ Cohort4ECommerce/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cohort4ECommerce.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +17,8 @@ namespace Cohort4ECommerce
         public void ConfigureServices(IServiceCollection services)
         {
 			services.AddMvc();
+
+			services.AddScoped<ICMSRepo, DevCMSRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
