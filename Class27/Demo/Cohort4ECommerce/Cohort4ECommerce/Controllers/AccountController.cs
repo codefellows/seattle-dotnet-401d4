58fa1f4ff8d6a01228ea3a701b72b9e39435a613
Iverson, Amanda <amanda@codefellows.com>
2018-07-10 19:25:25 -0700

    add Class 27 Demo Code

---
+++ Class27/Demo/Cohort4ECommerce/Cohort4ECommerce/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cohort4ECommerce.Models;
+using Cohort4ECommerce.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cohort4ECommerce.Controllers
+{
+	public class AccountController : Controller
+	{
+		private UserManager<ApplicationUser> _userManager;
+		private SignInManager<ApplicationUser> _signInManager;
+
+		public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel rvm)
+		{
+
+			var user = new ApplicationUser
+			{
+				UserName = rvm.Email,
+				Email = rvm.Email,
+				FirstName = rvm.FirstName,
+				LastName = rvm.LastName,
+				Birthday = rvm.Birthday
+			};
+
+			var result = await _userManager.CreateAsync(user, rvm.Password);
+
+			if(result.Succeeded)
+			{
+
+			}
+			return View();
+		}
+	}
+}
---
+++ Class27/Demo/Cohort4ECommerce/Cohort4ECommerce/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cohort4ECommerce.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime Birthday { get; set; }
+
+		[Display(Name = "Password")]
+
+		public string Password { get; set; }
+
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+
+		public string ConfirmPassword { get; set; }
+
+	}
+}
